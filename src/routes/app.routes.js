751061af78a9f43028720e8e8a03dde409f0e1d3
Criss <crxstxxp@icloud.com>
2024-03-20 20:54:21 -0600

    refactor(routes): add comments

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,17 +4,36 @@ const router = express.Router();
 
 const usersCtrl = require("../controllers/usersController");
 const spacesCtrl = require("../controllers/spacesController");
-const cardsCtrl = require('../controllers/cardsController');
+const cardsCtrl = require("../controllers/cardsController");
 
 router.post("/user/auth", usersCtrl.login);
+/**
+ * {
+  "nombre": "criss",
+  "password": "123"
+}
+ */
 
 //Crear espacios de trabajo (sidebar)
 router.post("/spaces", usersCtrl.verifyJWT, spacesCtrl.store);
+/**
+ * {
+ *  "name": "nombre ejemplo"
+ * }
+ */
 //Obtener los espacios de trabajo (sidebar)
 router.get("/spaces", usersCtrl.verifyJWT, spacesCtrl.index);
 
-//Crear las cartas de un espacio de trabajo por spaceId 
+//Crear las cartas de un espacio de trabajo por spaceId
 router.post("/spaces/:id/cards", usersCtrl.verifyJWT, cardsCtrl.store);
+/**
+ * formData()
+ * {
+ *  "title": "ejemplo titulo",
+ *  "description": "ejemplo desc"
+ *      file: archivo
+ * }
+ */
 //Obtener las cartas de un espacio de trabajo spaceId
 router.get("/spaces/:id/cards", usersCtrl.verifyJWT, cardsCtrl.index);
 
