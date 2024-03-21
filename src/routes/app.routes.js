fff719db9e0b7156c8df6295130ac415c9f04318
Criss <crxstxxp@icloud.com>
2024-03-20 20:47:09 -0600

    refactor(routes): add comments

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -8,10 +8,18 @@ const cardsCtrl = require('../controllers/cardsController');
 
 router.post("/user/auth", usersCtrl.login);
 
+//Crear espacios de trabajo (sidebar)
 router.post("/spaces", usersCtrl.verifyJWT, spacesCtrl.store);
+//Obtener los espacios de trabajo (sidebar)
 router.get("/spaces", usersCtrl.verifyJWT, spacesCtrl.index);
 
+//Crear las cartas de un espacio de trabajo por spaceId 
 router.post("/spaces/:id/cards", usersCtrl.verifyJWT, cardsCtrl.store);
+//Obtener las cartas de un espacio de trabajo spaceId
 router.get("/spaces/:id/cards", usersCtrl.verifyJWT, cardsCtrl.index);
+
+/**
+ * ex: /spaces/1/cards
+ */
 
 module.exports = router;
